4087d819f9872d0b0837fcb7e95c3f7ed29efb8b
Slees202 <64414794+Slees202@users.noreply.github.com>
2022-08-23 23:14:58 +0100

    Add files via upload

---
+++ app/src/main/java/org/stuart/gcu/leesstuartmpdresit/WeatherItems.java
@@ -0,0 +1,61 @@
+//Stuart Lees
+// S1821982
+
+package org.stuart.gcu.leesstuartmpdresit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherItems extends AppCompatActivity implements View.OnClickListener {
+    TextView viewtitle;
+    TextView viewtemperature;
+    TextView viewwind_dir;
+    TextView viewpressure;
+    TextView viewsunset;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.campusview);
+
+
+
+        Intent intent = getIntent();
+        viewtitle = findViewById(R.id.Weather);
+        viewtemperature = findViewById(R.id.MinimumTemperature);
+        viewwind_dir = findViewById(R.id.WindDirection);
+        viewpressure = findViewById(R.id.Pressure);
+        viewsunset = findViewById(R.id.Sunset);
+
+
+
+
+        String title = intent.getExtras().getString("title");
+        String min_temp = intent.getExtras().getString("min_temp");
+        String wind_dir = intent.getExtras().getString("wind_dir");
+        String pressure = intent.getExtras().getString("pressure");
+        String sunset = intent.getExtras().getString("sunset");
+
+
+
+
+        viewtitle.setText(title);
+        viewtemperature.setText(min_temp);
+        viewwind_dir.setText(wind_dir);
+        viewpressure.setText(pressure);
+        viewsunset.setText(sunset);
+
+    }
+
+        public void onClick (View view){
+        }
+    }
+
+
+
